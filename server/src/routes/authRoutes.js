27ab556d88017a6c2ac8c799806478b12e33b9ff
Kumar Prakhar <kumarprakharkp143@gmail.com>
2026-04-23 00:39:20 +0530

    backend auth logic

---
+++ server/src/routes/authRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import * as authController from '../controllers/authController.js';
+
+const router = express.Router();
+
+// Admin Auth
+router.post('/admin/login', authController.adminLogin);
+
+// Vendor Auth
+router.post('/vendor/login', authController.vendorLogin);
+router.post('/vendor/signup', authController.vendorSignup);
+
+// User Auth
+router.post('/user/login', authController.userLogin);
+router.post('/user/signup', authController.userSignup);
+
+export default router;
